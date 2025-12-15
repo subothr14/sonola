582f2530885a62a56427ea6d7fc87ef506e21bb4
subothr14 <subothr14@gmail.com>
2025-12-15 16:35:29 +0530

    add some content in project page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,27 +1,43 @@
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
+import { Recent } from "../lib/Dataset";
+import CardItem from "../components/CardItem";
 
 function Projects() {
   return (
     <AnimatePresence>
-      <motion.div className="flex h-screen items-center justify-center" 
-      initial={{
-        opacity:0,
-        
-      }}
-
-      animate={{
-        opacity:1,
-        y:0
-      }}
-
-      transition={{
-        duration:1
-      }}
+      <motion.div
+        className="flex h-screen  p-3"
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+        }}
       >
-        <h2 className="text-3xl">
-          Projects
-        </h2>
+        <div className="flex flex-col gap-3.5">
+          <h2 className="text-3xl font-black">Projects</h2>
+
+          <div>
+            <div className="pt-5">
+              <div className="grid grid-cols-2 xl:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {Recent.map((item) => (
+                  <div key={item.id}>
+                    <CardItem
+                      key={item.id}
+                      //@ts-ignore
+                      item={item}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </AnimatePresence>
   );
